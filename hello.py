9184b1d115cbdbd7eab7d48a13b0625e5a83dcb3
bjet2 <42326577+bjet2@users.noreply.github.com>
2019-09-21 18:52:16 -0400

    commit on github side

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@
 # Used with Arduino program Processing_accel
 # This program was originally used with Processing language
 #    before I got to frustrated with its unexpected quirks
-#Working on statup problem ....
-# ???
+# Change on git hub side ...
+
 
 """
 	Program to read data supplied by Arduino over serial port

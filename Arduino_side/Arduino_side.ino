c11b894a57b3bb37cd4f1d5b259fc5840e73f357
bjet2 <baynehenry@gmail.com>
2019-09-21 18:15:13 -0400

    Arduino and Python file for Accelerometer project

---
+++ Arduino_side/Arduino_side.ino
@@ -0,0 +1,66 @@
+#include <Wire.h>
+/* Accelerometers to send data to Processing
+ *  
+ *  I2C address ... ADXL345 = 0x53
+ * 
+ */
+ 
+
+const int ADXL345 = 0x53; // The ADXL345 sensor I2C address
+
+
+void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+//  digitalWrite(12,HIGH);
+//  delay(5000);
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  
+  Serial.begin(9600); // start serial communication
+  Wire.begin();       // start I2C communication
+
+  // Send settings to accel_0_port
+  Wire.beginTransmission(ADXL345); // Start communicating with the device 
+  Wire.write(0x2D); // Access/ talk to POWER_CTL Register - 0x2D
+                    // Enable measurement
+  Wire.write(8); // (8dec -> 0000 1000 binary) Bit D3 High for measuring enable 
+  Wire.endTransmission();
+  delay(100);
+    // Set the register to be read in ADXL345
+  Wire.beginTransmission(ADXL345); 
+  Wire.write(0x32); // Set the Register to be read 
+  Wire.endTransmission(false);
+  delay(100); 
+  digitalWrite(12,HIGH);
+  
+
+}
+
+void loop() {
+  
+  float accelx, accely, accelz;
+
+  Wire.beginTransmission(ADXL345); 
+  Wire.write(0x32); // Set the Register to be read 
+  Wire.endTransmission(false);
+  digitalWrite(12,HIGH); 
+ 
+  Wire.requestFrom(ADXL345, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
+  accelx = float(Wire.read()|Wire.read() << 8); 
+  accely = float(Wire.read()|Wire.read() << 8); 
+  accelz = float(Wire.read()|Wire.read() << 8); 
+  //Serial.print("d,"); 
+  Serial.print(accelx);
+  Serial.print(',');
+  Serial.print(accely);
+  Serial.print(',');
+  Serial.print(accelz);
+  unsigned long pTime = millis();
+  float t= float(pTime)/1000;
+  Serial.print(',');
+  Serial.println(t);      // NOTE: ln very important for Processing
+  delay(100);
+}
+
+  
---
+++ hello.py
@@ -0,0 +1,38 @@
+# My first python program. Reads data from Arduino and parses it into numerial data
+# Used with Arduino program Processing_accel
+# This program was originally used with Processing language
+#    before I got to frustrated with its unexpected quirks
+
+"""
+	Program to read data supplied by Arduino over serial port
+	Arduino is connected to ADXL345 accelerometer via R2C.
+	 
+	Arduino will send data as strings with the following form
+	
+	ax,ay,az,time_in_millis
+	
+"""
+
+# create serial object called arduinoSerialData
+import serial										# Serial library
+arduinoSerialData = serial.Serial('/dev/ttyACM0',9600)			
+
+for i in range(0,100):							# for loop to limit time program runs
+	
+												# do nothing ...wait for data from Arduino (threading here??)
+	while (arduinoSerialData.inWaiting()==0):
+		pass										
+	
+	
+	myData = arduinoSerialData.readline()		# get the data string
+	print(myData)								# print the string
+	myData = myData.decode('utf-8')				# not an issue in Python2 but in Python3 this decode must be done
+												# see https://stackoverflow.com/questions/31529421/weird-output-value-bvalue-r-n-python-serial-read
+	myData = myData.strip()						# strip all non-character and white spaces from right side of myData
+	print(myData)								# display stripped data 
+	data = myData.split(",")					# Split data using "," as delimiter
+	print(data)									# Print the split data
+	ax = 0.0									# convert data string into float
+	ax = float(data[0])
+	print(ax)									# display acceleration ax as test
+

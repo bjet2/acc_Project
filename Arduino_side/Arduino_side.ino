525a230ab0a6b359de3b479421283188efd9585d
bjet2 <baynehenry@gmail.com>
2019-09-29 09:43:39 -0400

    error corrected. accel and gyro data now on arduino side

--- Arduino_side/Arduino_side.ino
+++ Arduino_side/Arduino_side.ino
@@ -31,6 +31,10 @@ void setup() {
       //Serial.println("0.0,0.0,0.0,0.0");
   }
 }
+
+
+/*************************************************************************************************************/
+// Main loop
 /*************************************************************************************************************/
 void loop() {
   uint8_t ADXL345 = 0x53, ADXL345_data_register = 0x32;
@@ -41,50 +45,46 @@ void loop() {
   float scale[3] = {0.935870 , 0.930660, 1.015939};
   float offset[3] = {9.5,-18,-36};
   float gyro[3];
-  float gyro_offset[3]={17641.1,-17465.6,-18680.1};
+  float gyro_offset[3]={-450.0,-170.0,-70.0};
+
+  //*******************************************************************************
+  // Coding for Accelerometer and time:   ax,ay,az,t   sent to Serial port
+  //*******************************************************************************
+
   b.i2cReadBytes(ADXL345, ADXL345_data_register , raw_data,6);
   for(int i=0;i<3;i++){
     int j,k;
     j = i*2;
     k=j+1;
-    accel[i] = float(raw_data[k]|raw_data[j]<<8);
+    accel[i] = float(raw_data[j]|raw_data[k]<<8);
     accel[i] = (accel[i] + offset[i])*scale[i];
     accel[i] = accel[i]*9.8/255;
-    //Serial.print(accel[i]); Serial.print(',');
+    Serial.print(accel[i]); Serial.print(',');
   }
   unsigned long pTime = millis();
   float t= float(pTime)/1000;
-  //Serial.println(t);      // NOTE: println very important for Python program to have serial data with end line
+  Serial.print(t);      // NOTE: println very important for Python program to have serial data with end line
 
-//*******************************************************************************
-// TODO: Adding code here for Gyro
-//*******************************************************************************
+  //*******************************************************************************
+  // Coding for Gyro: ,gx,gy,gz   sent to Serial port
+  //*******************************************************************************
 
   b.i2cReadBytes(MPU3050, MPU3050_data_register , raw_data,6);
-  //Serial.print("RAW DATA MPU3050>>>>  ");
-
-  
   for(int i=0;i<3;i++){
     int j,k;
     j = i*2;
     k=j+1;
     gyro[i] = float(raw_data[k]|raw_data[j]<<8);
-    gyro[i] = (gyro[i] - gyro_offset[i])/131;
-    //Serial.print(gyro[i]); Serial.print(',');
+    gyro[i] = (gyro[i] - gyro_offset[i])/131;   // see notes as to 131 scaling ... linked to full scale setting
+    Serial.print(","); Serial.print(gyro[i]);
   }
-  //Serial.println("  <<<<RAW DATA MPU3050");
-  Serial.print(gyro[0]);
-  Serial.print(",");
-  Serial.print(gyro[1]);
-  Serial.print(",");
-  Serial.println(gyro[2]);
+  Serial.println();
 
-  delay(10);
 
   
-//*******************************************************************************
-// end Gyro coding construction 
-//*******************************************************************************
+  //*******************************************************************************
+  // TODO: Calibrations is rough...redo with more accuracy
+  //*******************************************************************************
 }
 
 /*************************************************************************************************************/
